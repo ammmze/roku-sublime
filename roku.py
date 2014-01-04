d586ce262e1539d6faf76007f09c9f41624e5eda
Branden Cash <ammmze@gmail.com>
2014-01-04 12:42:47 -0700

    Rather than parsing the json manually, lets just use the project_data dictionary provided by sublime text...note: we should add some error handling if the project file doesn't exist, and perhaps default to a preference setting vs project setting

--- roku.py
+++ roku.py
@@ -3,129 +3,120 @@ import os, sys
 import subprocess
 import re
 import zipfile
-import json
 
 def get_root_directory(self):
 	folders = self.window.folders()
-	
+
 	projDir = ""
 	for folder in folders:
-		
+
 		folderLen = len(folder.split(os.sep))
-		
+
 		if projDir == "" or folderLen < len(projDir.split(os.sep)):
 			projDir = folder
-			
+
 	return projDir
 
 
 def get_project_name(projDir):
 	dirList = projDir.split(os.sep)
 	return dirList[len(dirList)-1]
-	
-	
+
+
 class RokuPackageCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		print('Packaging...')
-		
+
 		projectDir = get_root_directory(self)
 		projectName = get_project_name(projectDir)
 		outputDirectory = projectDir + os.sep + 'build'
 		ouputZip = outputDirectory + os.sep + projectName
-		
+
 		# check for and create build directory if necessary
 		if not os.path.exists(outputDirectory):
 			os.makedirs(outputDirectory)
-		
+
 		print('Zipping ' + projectDir)
-		
+
 		zf = zipfile.ZipFile('%s.zip' % (ouputZip), 'w')
 		abs_src = os.path.abspath(projectDir)
-		
+
 		for dirname, subdirs, files in os.walk(projectDir):
 			for filename in files:
 				absname = os.path.abspath(os.path.join(dirname, filename))
 				arcname = absname[len(abs_src) + 1:]
-				
-				# This is fairly messy need to fix this 
+
+				# This is fairly messy need to fix this
 				#	- maybe set up preferences for what to ignore in the project file or prefs file
-				if(arcname.find('.buildpath') == -1 and 
-					arcname.find('.gitignore') == -1 and 
+				if(arcname.find('.buildpath') == -1 and
+					arcname.find('.gitignore') == -1 and
 					arcname.find('.project') == -1 and
-					arcname.find('.git') == -1 and 
+					arcname.find('.git') == -1 and
 					arcname.find('.sublime') == -1 and
 					arcname.find('build') == -1):
 						print('zipping %s as %s' % (os.path.join(dirname, filename), arcname))
 						zf.write(absname, arcname)
 		zf.close()
-			
+
 		print('packaging complete.')
 
 # need tofigure out how to load a settings file so we can get the ip addy
 class RokuInstallCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		print('Installing...')
-		
+
 		projectDir = get_root_directory(self)
 		outputDirectory = projectDir + os.sep + 'build'
 		projectName = get_project_name(projectDir)
 		archive = outputDirectory + os.sep + projectName + '.zip'
-		projectJson = open(projectDir + os.sep + projectName + '.sublime-project')
-		
-		# need to add error handling for json parsing.
-		projectSettings = json.load(projectJson)
-		
+
 		print('Uploading... ' + archive)
-			
+
 		result = subprocess.Popen([
-			'curl', 
-			'-F', 'archive=@' + archive + ';type=application/zip', 
+			'curl',
+			'-F', 'archive=@' + archive + ';type=application/zip',
 			'-F', 'mysubmit=Install', '-H',
 			'Content-type:multipart/form-data',
-			'http://' + projectSettings.get('ip') + '/plugin_install/'],
+			'http://' + sublime.active_window().project_data()['ip'] + '/plugin_install/'],
 			stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		
+
 		output, errors = result.communicate()
 		if result.returncode:
 			raise Exception(errors)
-		else: 
+		else:
 			print(output)
-		
+
 		print('installation complete.')
 
 
 class RokuReplaceCommand(sublime_plugin.ApplicationCommand):
 	def run(args):
 		print('Replacing...')
-		
+
 		projectDir = get_root_directory(self)
 		outputDirectory = projectDir + os.sep + 'build'
 		projectName = get_project_name(projectDir)
 		archive = outputDirectory + os.sep + projectName + '.zip'
-		projectJson = open(projectDir + os.sep + projectName + '.sublime-project')
-		
-		# need to add error handling for json parsing.
-		projectSettings = json.load(projectJson)
-		
+
 		print('Uploading... ' + archive)
-			
+
 		result = subprocess.Popen([
-			'curl', 
-			'-F', 'archive=@' + archive + ';type=application/zip', 
+			'curl',
+			'-F', 'archive=@' + archive + ';type=application/zip',
 			'-F', 'mysubmit=Replace', '-H',
 			'Content-type:multipart/form-data',
-			'http://' + projectSettings.get('ip') + '/plugin_install/'],
+			'http://' + sublime.active_window().project_data()['ip'] + '/plugin_install/'],
 			stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		
+
 		output, errors = result.communicate()
 		if result.returncode:
 			raise Exception(errors)
-		else: 
+		else:
 			print(output)
-		
+
 		print('replacement complete.')
 
-	
+
 # class RokuRunProcessCommand(sublime_plugin.ApplicationCommand):
 # 	def run(args):
 # 		print('Start running a script...')
@@ -133,10 +124,10 @@ class RokuReplaceCommand(sublime_plugin.ApplicationCommand):
 # 		output, errors = result.communicate()
 # 		if result.returncode:
 # 			raise Exception(errors)
-# 		else: 
+# 		else:
 # 			print(output)
 # 		print('...Script complete')
-		
-	
-		
- 
+
+
+
+
